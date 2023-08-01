9a58818eaf50b528252daeeef2b75f1c01ad2d7d
durga2677 <125868369+durga2677@users.noreply.github.com>
2023-08-01 12:59:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function highlight() {
     //Write your code here
+	var boldWords = document.querySelectorAll('strong')
+  boldWords.forEach(function(word) {
+    word.style.color = 'green';
+  });
 
 
 }
@@ -7,6 +11,10 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
+	var boldWords = document.querySelectorAll('strong')
+  boldWords.forEach(function(word) {
+    word.style.color = 'black';
+  });
 
     
 }
